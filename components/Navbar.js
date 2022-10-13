2e396ab60e1002034c99bd7fee8da95cd96b7082
Joel Edem Amenuvor <jaamen5@gmail.com>
2022-10-13 19:13:06 +0000

    added homepage

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,11 @@ import SearchIcon from "@mui/icons-material/Search";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const { data: session, status } = useSession();
+  const user = session?.user?.email;
   return (
     <div className="border-b border-b-gray-400 shadow-md  min-w-screen ">
       <nav className=" flex justify-between py-2 max-w-7xl mx-auto px-4 ">
@@ -17,26 +19,36 @@ const Navbar = () => {
           </Link>
           <span className="text-gray-500 font-semibold">Raven</span>
         </div>
-        <div className="rounded-lg w-[60%] border-gray-400 bg-violet-100 sm:w-[35%] md:w-[40%] lg:[60%]  ">
-          <SearchIcon className="text-violet-500" />
-          <input
-            type="text"
-            placeholder="Search Mail"
-            className="outline-0 bg-transparent w-[80%]"
-          />
-        </div>
+        {user ? (
+          <div className="rounded-lg w-[60%] border-gray-400 bg-violet-100 sm:w-[35%] md:w-[40%] lg:[60%]  ">
+            <SearchIcon className="text-violet-500" />
+            <input
+              type="text"
+              placeholder="Search Mail"
+              className="outline-0 bg-transparent w-[80%]"
+            />
+          </div>
+        ) : (
+          ""
+        )}
 
         <div className="flex gap-3 cursor-pointer">
-          <span className="font-serif font-semibold text-gray-700">
-            Hi Joel!
-          </span>
-          <Link href="/profile">
-            <AccountCircleIcon className="text-violet-500 " />
-          </Link>
-          <LogoutIcon
-            className="text-violet-500 "
-            onClick={() => signOut({ callbackUrl: "/auth/login" })}
-          />
+          <span className="font-serif font-semibold text-gray-700">{user}</span>
+          {user ? (
+            <Link href="/profile">
+              <AccountCircleIcon className="text-violet-500 " />
+            </Link>
+          ) : (
+            ""
+          )}
+          {user ? (
+            <LogoutIcon
+              className="text-violet-500 "
+              onClick={() => signOut({ callbackUrl: "/auth/login" })}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </nav>
     </div>
